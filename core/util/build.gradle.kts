ebc48f7841955a9e7c4ec276877a6d36880fecde
Tsutsurin Kirill <kirill.cucurin@gmail.com>
2023-09-16 19:50:42 +0700

    delete extra Java version

--- core/util/build.gradle.kts
+++ core/util/build.gradle.kts
@@ -3,11 +3,6 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
-}
-
 dependencies {
     implementation(libs.kotlinx.coroutines.core)
 }
